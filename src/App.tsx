89a472b916faef16a65bd5b73232d9b8a49aec1e
Minh Phan <pbminh2511@gmail.com>
2020-08-29 18:02:42 +0700

    Add react-diagrams

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from "react";
+import createEngine, { 
+  DefaultLinkModel, 
+  DefaultNodeModel,
+  DiagramModel 
+} from '@projectstorm/react-diagrams';
 
+import {
+  CanvasWidget
+} from '@projectstorm/react-canvas-core';
+
+const engine = createEngine();
+const node1 = new DefaultNodeModel({
+  name: 'Node 1',
+  color: 'rgb(0,192,255)',
+});
+node1.setPosition(100, 100);
+let port1 = node1.addOutPort('Out');
+
+// node 2
+const node2 = new DefaultNodeModel({
+  name: 'Node 1',
+  color: 'rgb(0,192,255)',
+});
+node2.setPosition(100, 100);
+let port2 = node2.addOutPort('Out');
+const link = port1.link<DefaultLinkModel>(port2);
+link.addLabel('Hello World!');
+const model = new DiagramModel();
+model.addAll(node1, node2, link);
+engine.setModel(model);
 function App() {
   return (
-    <div>
-      <h1>☆*:.｡.o(≧▽≦)o.｡.:*☆</h1>
-    </div>
+    <CanvasWidget engine={engine} className="canvas"/>
   );
 }
 
