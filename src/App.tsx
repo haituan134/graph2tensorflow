e3c10ff21625e7aea5f3bcd71beaba49b0ba4087
Lam Nguyen <lamnhh.cs@gmail.com>
2020-08-30 12:50:25 +0700

    Fix prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import React from "react";
-import createEngine, { 
-  DefaultLinkModel, 
+import createEngine, {
+  DefaultLinkModel,
   DefaultNodeModel,
-  DiagramModel 
-} from '@projectstorm/react-diagrams';
+  DiagramModel,
+} from "@projectstorm/react-diagrams";
 
-import {
-  CanvasWidget
-} from '@projectstorm/react-canvas-core';
+import { CanvasWidget } from "@projectstorm/react-canvas-core";
 
 const engine = createEngine();
 const node1 = new DefaultNodeModel({
-  name: 'Node 1',
-  color: 'rgb(0,192,255)',
+  name: "Node 1",
+  color: "rgb(0,192,255)",
 });
 node1.setPosition(100, 100);
-let port1 = node1.addOutPort('Out');
+let port1 = node1.addOutPort("Out");
 
 // node 2
 const node2 = new DefaultNodeModel({
-  name: 'Node 1',
-  color: 'rgb(0,192,255)',
+  name: "Node 1",
+  color: "rgb(0,192,255)",
 });
 node2.setPosition(100, 100);
-let port2 = node2.addOutPort('Out');
+let port2 = node2.addOutPort("Out");
 const link = port1.link<DefaultLinkModel>(port2);
-link.addLabel('Hello World!');
+link.addLabel("Hello World!");
 const model = new DiagramModel();
 model.addAll(node1, node2, link);
 engine.setModel(model);
 function App() {
-  return (
-    <CanvasWidget engine={engine} className="canvas"/>
-  );
+  return <CanvasWidget engine={engine} className="canvas" />;
 }
 
 export default App;
