7490f83f16235ef582ee53c7b06fb8bb5294c2fa
Lam Nguyen <lamnhh.cs@gmail.com>
2020-09-02 20:21:41 +0700

    Add constraint on link

--- src/models/LayerPortModel.ts
+++ src/models/LayerPortModel.ts
@@ -6,15 +6,34 @@ import {
 } from "@projectstorm/react-diagrams";
 
 export class LayerPortModel extends PortModel {
+  portType: "in" | "out";
+
   constructor(alignment: PortModelAlignment) {
     super({
       type: "layer",
       name: alignment,
       alignment,
     });
+    this.portType = alignment === PortModelAlignment.LEFT ? "in" : "out";
   }
 
   createLinkModel(): LinkModel {
     return new DefaultLinkModel();
+  }
+
+  canLinkToPort(port: LayerPortModel) {
+    // Only allow link from outPort to inPort
+    if (this.portType !== "out" || port.portType !== "in") {
+      return false;
+    }
+    // An inPort can have at most 1 link
+    if (Object.keys(port.links).length >= 1) {
+      return false;
+    }
+    // TODO: remove this to allow a layer output to be used in multiple places
+    if (Object.keys(this.links).length >= 2) {
+      return false;
+    }
+    return true;
   }
 }
